1514b7461ce8bd121e93b2dc8d7edb73b5b2571d
sharath50 <sharath50@users.noreply.github.com>
2019-07-06 13:37:26 +0530

    modified some code put arrow functions

--- js/script.js
+++ js/script.js
@@ -32,59 +32,55 @@ function submitData() {
 
 function loadData(url) {
     
-    // create a ajax connection
-    const xhr = new XMLHttpRequest();
-    
-    try {    //get the data from the url..
-        xhr.open('GET' , url , true);
-
-        //read the data from the response
-        xhr.onload = function() {
-            if (this.status === 200 && this.status < 400 ) {
-                // convert it into object...
-                const names = JSON.parse(this.responseText);
-
-                let content = "<h2 class='h2'>Generated names</h2>";
-                content += "<ul class='list-unstyled'>"
-                names.forEach( (name) => {
-                   content += `<li class='list-item'>${name.name}</li>`;
-                });
-                content += "</ul>";
-                document.getElementById("result").innerHTML = content;
-            } else {
-                document.getElementById("result").innerHTML = "<h4 class'h4'>Resource Limit Reached, try again</h4>";
-            }
-
-
-
-        }
-    }
-    finally {
-        document.getElementById("result").innerHTML = "<h4 class'h4'>Resource Limit Reached, try again</h4>";
-    }
-    
-    //send the ajax request
-    xhr.send();
-    
-    
-    
-    // by using fetch api
-//    fetch(url)
-//    .then( (response) => {
-//        return response.json();
-//    })
-//    .then( (names) => {
-//        let content = "<h2 class='h2'>Generated names</h2>";
-//        content += "<ul class='list-unstyled'>"
-//        names.forEach( (name) => {
-//           content += `<li class='list-item'>${name.name}</li>`;
-//        });
-//        content += "</ul>";
-//        document.getElementById("result").innerHTML = content;
-//    })
-//    .catch( (error) => {
+//    // create a ajax connection
+//    const xhr = new XMLHttpRequest();
+//    
+//    try {    //get the data from the url..
+//        xhr.open('GET' , url , true);
+//
+//        //read the data from the response
+//        xhr.onload = () => {
+//            if (this.status === 200 && this.status < 400 ) {
+//                // convert it into object...
+//                const names = JSON.parse(this.responseText);
+//
+//                let content = "<h2 class='h2'>Generated names</h2>";
+//                content += "<ul class='list-unstyled'>"
+//                names.forEach( (name) => {
+//                   content += `<li class='list-item'>${name.name}</li>`;
+//                });
+//                content += "</ul>";
+//                document.getElementById("result").innerHTML = content;
+//            } else {
+//                document.getElementById("result").innerHTML = "<h4 class'h4'>Resource Limit Reached, try again</h4>";
+//            }
+//
+//
+//
+//        }
+//    }
+//    finally {
 //        document.getElementById("result").innerHTML = "<h4 class'h4'>Resource Limit Reached, try again</h4>";
-//    })
+//    }
+//    
+//    //send the ajax request
+//    xhr.send();
+    
+    
+    
+//    // by using fetch api
+    fetch(url)
+    .then( response => response.json()) // we can write arrow function in single line without Parenthesis for single parameters 
+    .then( names => {
+        let content = "<h2 class='h2'>Generated names</h2>";
+        content += "<ul class='list-unstyled'>"
+        names.forEach( (name) => {
+           content += `<li class='list-item'>${name.name}</li>`;
+        });
+        content += "</ul>";
+        document.getElementById("result").innerHTML = content;
+    })
+    .catch( error => document.getElementById("result").innerHTML = "<h4 class'h4'>Resource Limit Reached, try again</h4>" )
     
 }
 
